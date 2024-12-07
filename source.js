a8469a0fda1be1cd9824a9bb26b7e840418c0a77
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2024-12-06 23:26:45 -0600

    Added JS, more images.

---
+++ source.js
@@ -0,0 +1,110 @@
+function discord() {
+    document.getElementById("updatingInfo").innerText = 
+    "Between February 2020 and May 2022, I actively developed and maintained a Discord Web Application (Bot) used in over 4,500+ servers by thousands of users."
+    + " Below are different features I've developed for the bot. The bot was programmed in Node.js and was later watered down and compiled into a open source GitHub repo.";
+    let img1 = document.getElementById("updatingImg1")
+    img1.src = "Images/Solitaire.png";
+    img1.style.visibility = "visible"
+    img1.style.width = '350px'
+    let img2 = document.getElementById("updatingImg2")
+    img2.src = "Images/Pets.png";
+    img2.style.visibility = "visible"
+    let img3 = document.getElementById("updatingImg3")
+    img3.src = "Images/Sharding.png";
+    img3.style.visibility = "visible"
+    let img4 = document.getElementById("updatingImg4")
+    img4.src = "Images/RedditAPI.png";
+    img4.style.visibility = "visible"
+    let img5 = document.getElementById("updatingImg5")
+    img5.src = "Images/SlidePuzzle.png";
+    img5.style.visibility = "visible"
+}
+
+function toDoList() {
+    document.getElementById("updatingInfo").innerText = 
+    "Below is a simple HTML/JS to do list project."
+    + " It is open sourced on my GitHub.";
+
+    let img1 = document.getElementById("updatingImg1")
+    img1.src = "Images/ToDoList.png";
+    img1.style.visibility = "visible"
+    img1.style.width = '750px'
+
+    let img2 = document.getElementById("updatingImg2")
+    img2.style.visibility = "collapse"
+    let img3 = document.getElementById("updatingImg3")
+    img3.style.visibility = "collapse"
+    let img4 = document.getElementById("updatingImg4")
+    img4.style.visibility = "collapse"
+    let img5 = document.getElementById("updatingImg5")
+    img5.style.visibility = "collapse"
+}
+
+function cyberDefense() {
+    document.getElementById("updatingInfo").innerText = 
+    "I am apart of UW-Stout's Collegiate Cyber Defense Competition."
+    + " I research security vulnurabilities and actively threat hunt during competitions, compiling my findings into incident reports."
+    + " My main focus is a Debian DNS/NTP server."
+
+    let img1 = document.getElementById("updatingImg1")
+    img1.style.visibility = "hidden"
+    let img2 = document.getElementById("updatingImg2")
+    img2.style.visibility = "collapse"
+    let img3 = document.getElementById("updatingImg3")
+    img3.style.visibility = "collapse"
+    let img4 = document.getElementById("updatingImg4")
+    img4.style.visibility = "collapse"
+    let img5 = document.getElementById("updatingImg5")
+    img5.style.visibility = "collapse"
+}
+
+function codingComp() {
+    document.getElementById("updatingInfo").innerText = "codingComp";
+
+    let img1 = document.getElementById("updatingImg1")
+    img1.style.visibility = "hidden"
+    let img2 = document.getElementById("updatingImg2")
+    img2.style.visibility = "collapse"
+    let img3 = document.getElementById("updatingImg3")
+    img3.style.visibility = "collapse"
+    let img4 = document.getElementById("updatingImg4")
+    img4.style.visibility = "collapse"
+    let img5 = document.getElementById("updatingImg5")
+    img5.style.visibility = "collapse"
+}
+
+function oAuth() {
+    document.getElementById("updatingInfo").innerText = 
+    "As a part of UW-Stout's Information Security Professionals, I presented OAuth. OAuth is a protocol for sharing resources between services as authorized by an end user."
+    + " I dsiplayed a demo of how it can be implemented, misuse of it, and vulnurabilites present in OAuth.";
+
+    let img1 = document.getElementById("updatingImg1")
+    img1.src = "Images/Oauth.png";
+    img1.style.width = '350px'
+    img1.style.visibility = "visible"
+    let img2 = document.getElementById("updatingImg2")
+    img2.src = "Images/code.png";
+    img2.style.visibility = "visible"
+    let img3 = document.getElementById("updatingImg3")
+    img3.style.visibility = "hidden"
+    let img4 = document.getElementById("updatingImg4")
+    img4.style.visibility = "hidden"
+    let img5 = document.getElementById("updatingImg5")
+    img5.style.visibility = "hidden"
+}
+
+function malwareAnalysis() {
+    document.getElementById("updatingInfo").innerText = 
+    "As a part of UW-Stout's Honors College, I am tasked with working on a personal project";
+
+    let img1 = document.getElementById("updatingImg1")
+    img1.style.visibility = "hidden"
+    let img2 = document.getElementById("updatingImg2")
+    img2.style.visibility = "hidden"
+    let img3 = document.getElementById("updatingImg3")
+    img3.style.visibility = "hidden"
+    let img4 = document.getElementById("updatingImg2")
+    img4.style.visibility = "hidden"
+    let img5 = document.getElementById("updatingImg3")
+    img5.style.visibility = "hidden"
+}
