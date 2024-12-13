151bf82ad8774360d2e23ca0406dd76e3f255476
FletcherMeyer <53588444+FletcherMeyer@users.noreply.github.com>
2024-12-12 20:26:36 -0600

    Added CCDC IRs

--- source.js
+++ source.js
@@ -1,7 +1,9 @@
+
 function discord() {
     document.getElementById("updatingInfo").innerText = 
     "Between February 2020 and May 2022, I actively developed and maintained a Discord Web Application (Bot) used in over 4,500+ servers by thousands of users."
-    + " Below are different features I've developed for the bot. The bot was programmed in Node.js and was later watered down and compiled into a open source GitHub repo.";
+    + "\n\nThe bot was programmed in Node.js using Discord's API. It was later watered down and compiled into a open source GitHub repo."
+    + "\n\nBelow are different features developed for the bot. My favorite is Solitaire, which stored record times, number of moves, and various custom card skins. Sharding used for distributed processing, databases used to store user's pets, and use of Reddit's API are also shown off."
     let img1 = document.getElementById("updatingImg1")
     img1.src = "Images/Solitaire.png";
     img1.style.visibility = "visible"
@@ -9,21 +11,30 @@ function discord() {
     let img2 = document.getElementById("updatingImg2")
     img2.src = "Images/Pets.png";
     img2.style.visibility = "visible"
+    img2.style.width = '350px'
+
     let img3 = document.getElementById("updatingImg3")
     img3.src = "Images/Sharding.png";
     img3.style.visibility = "visible"
+    img3.style.width = '350px'
+
     let img4 = document.getElementById("updatingImg4")
     img4.src = "Images/RedditAPI.png";
     img4.style.visibility = "visible"
+    img4.style.width = '350px'
+
     let img5 = document.getElementById("updatingImg5")
     img5.src = "Images/SlidePuzzle.png";
     img5.style.visibility = "visible"
-}
+    img5.style.width = '350px'
 
+}
+window.onload = function() {
+    discord();
+};
 function toDoList() {
     document.getElementById("updatingInfo").innerText = 
-    "Below is a simple HTML/JS to do list project."
-    + " It is open sourced on my GitHub.";
+    "Below is a simple HTML/JS to do list project.\n\nUsers can create, edit, drag around, or delete tasks on their to-do-list. Additionally, users can save their progress to their web browser's cookies.\n\nThe project is open sources on my GitHub.";
 
     let img1 = document.getElementById("updatingImg1")
     img1.src = "Images/ToDoList.png";
@@ -43,19 +54,30 @@ function toDoList() {
 function cyberDefense() {
     document.getElementById("updatingInfo").innerText = 
     "I am apart of UW-Stout's Collegiate Cyber Defense Competition."
-    + " I research security vulnurabilities and actively threat hunt during competitions, compiling my findings into incident reports."
-    + " My main focus is a Debian DNS/NTP server."
-
+    + "\n\nI research security vulnurabilities and actively threat hunt during competitions, compiling my findings into incident reports."
+    + "\n\nMy main focus is a Debian DNS/NTP server. I ensure end-user's can perform name resolution and receive the correct addres using Bind9."
+    + "\n\nBelow is an example incident report I created during practice."
     let img1 = document.getElementById("updatingImg1")
-    img1.style.visibility = "hidden"
+    img1.style.width = '750px'
+    img1.src = "Images/IR1.png";
+    img1.style.visibility = "visible"
     let img2 = document.getElementById("updatingImg2")
-    img2.style.visibility = "collapse"
+    img2.src = "Images/IR2.png";
+    img2.style.visibility = "visible"
+    img2.style.width = '750px'
     let img3 = document.getElementById("updatingImg3")
-    img3.style.visibility = "collapse"
+    img3.src = "Images/IR3.png";
+    img3.style.visibility = "visible"
+    img3.style.width = '750px'
     let img4 = document.getElementById("updatingImg4")
-    img4.style.visibility = "collapse"
+    img4.src = "Images/IR4.png";
+    img4.style.visibility = "visible"
+    img4.style.width = '750px'
     let img5 = document.getElementById("updatingImg5")
-    img5.style.visibility = "collapse"
+    img5.src = "Images/IR5.png";
+    img5.style.visibility = "visible"
+    img5.style.width = '750px'
+
 }
 
 function codingComp() {
@@ -75,8 +97,8 @@ function codingComp() {
 
 function oAuth() {
     document.getElementById("updatingInfo").innerText = 
-    "As a part of UW-Stout's Information Security Professionals, I presented OAuth. OAuth is a protocol for sharing resources between services as authorized by an end user."
-    + " I dsiplayed a demo of how it can be implemented, misuse of it, and vulnurabilites present in OAuth.";
+    "As a part of UW-Stout's Information Security Professionals, I presented information on the Open Authorization protocol. OAuth is a protocol for sharing resources between services as authorized by an end user."
+    + "\n\nI displayed a demo of how it can be implemented, modern misuse of it, and vulnurabilites present in OAuth.";
 
     let img1 = document.getElementById("updatingImg1")
     img1.src = "Images/Oauth.png";
@@ -84,6 +106,7 @@ function oAuth() {
     img1.style.visibility = "visible"
     let img2 = document.getElementById("updatingImg2")
     img2.src = "Images/code.png";
+    img2.style.width = '750px'
     img2.style.visibility = "visible"
     let img3 = document.getElementById("updatingImg3")
     img3.style.visibility = "hidden"
@@ -91,6 +114,9 @@ function oAuth() {
     img4.style.visibility = "hidden"
     let img5 = document.getElementById("updatingImg5")
     img5.style.visibility = "hidden"
+}
+
+function algorithms() {
 }
 
 function resume() {
